66d5a192aa107e85d63d61869577d610dd900d9c
Musab <malqarninet@gmail.com>
2023-05-01 17:21:53 +0300

    Implement contact section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css"
 import Banner from "./components/Banner";
+import Contact from "./components/Contact";
 import { NavBar } from './components/NavBar';
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
